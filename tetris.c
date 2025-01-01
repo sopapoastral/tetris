e8db8a1db584d98bc89d5c24a440b585dab76e9f
Julián Olivera <julian.olivera@fing.edu.uy>
2025-01-01 19:31:45 -0300

    SDL Window Test

--- tetris.c
+++ tetris.c
@@ -1,11 +1,22 @@
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_video.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+int running = 1;
+
+void
+handle_sigint (int sig)
+{
+    (void)sig;
+    running = 0;
+}
 
 int
 main (int argc, char *argv[])
 {
+    signal (SIGINT, handle_sigint);
+
     SDL_Window *win
         = SDL_CreateWindow ("my window", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
     // Window failed to create
@@ -16,6 +27,24 @@ main (int argc, char *argv[])
             exit (1);
         }
 
+    SDL_Surface *winSurface;
+    do
+        {
+            winSurface = SDL_GetWindowSurface (win);
+            SDL_UpdateWindowSurface (win);
+
+            SDL_FillRect (winSurface, NULL,
+                          SDL_MapRGB (winSurface->format, 255, 90, 120));
+
+            SDL_Delay (100);
+        }
+    while (running);
+
+    SDL_DestroyWindow (win);
+    win        = NULL;
+    winSurface = NULL;
+
+    SDL_Quit ();
 
     return 0;
 }
