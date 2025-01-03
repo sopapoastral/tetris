96aca5e7c0831a32a92e02c6b4be9a929fb6c955
Julián Olivera <julian.olivera@fing.edu.uy>
2025-01-02 23:49:19 -0300

    Circle Brush v1.2

--- tetris.c
+++ tetris.c
@@ -8,21 +8,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define REFRESH_RATE 1
+#define REFRESH_RATE 10
 #define D_NUEVO(x, y, d)                                                      \
     ((d) < 0 ? (d) + (2 * (x)) + 3 : (d) + (2 * (x)) - (2 * (y)) + 5)
+#define CIRCUMFERENCE(R) (2 * M_PI * (R))
+#define CIRCLE_AREA(R) (M_PI * pow ((R), 2))
 
 SDL_Window   *win;
 SDL_Renderer *renderer;
+SDL_Texture *texture;
 
 int           init (void);
 void          kill (void);
 int           loop (void);
 SDL_Point    *circle (int, int, int, int *);
+SDL_Point    *brush (int, int, int, int *);
+SDL_Point *(*shape) (int, int, int, int *);
 
 int
 main (int argc, char *argv[])
 {
+    shape = brush;
     if (!init ())
         return 1;
 
@@ -110,21 +116,29 @@ loop (void)
         SDL_SetRenderDrawColor (renderer, 255, 90, 120, 255);
         SDL_RenderClear (renderer);
     }
+    else if (keys[SDL_SCANCODE_Q])
+    {
+        return 0;
+    }
     else if (keys[SDL_SCANCODE_F4]
              && (keys[SDL_SCANCODE_LALT] || keys[SDL_SCANCODE_RALT]))
     {
         return 0;
     }
-
-    if (mx0 != -1)
+    else if (keys[SDL_SCANCODE_C])
     {
-        int        cant   = 0;
-        int x = ( mx1 != 0 ) ? mx1 : mx0;
-        int y = ( my1 != 0 ) ? my1 : my0;
-        SDL_Point *points = circle (x, y, 30, &cant);
-        SDL_RenderDrawPoints (renderer, points, cant);
-        free (points);
+        shape = (shape == circle) ? brush : circle;
     }
+
+        if (mx0 != -1)
+        {
+            int        cant   = 0;
+            int        x      = (mx1 != 0) ? mx1 : mx0;
+            int        y      = (my1 != 0) ? my1 : my0;
+            SDL_Point *points = shape (x, y, 30, &cant);
+            SDL_RenderDrawPoints (renderer, points, cant);
+            free (points);
+        }
 
     SDL_RenderPresent (renderer);
 
@@ -138,58 +152,3 @@ kill (void)
     SDL_DestroyWindow (win);
     SDL_Quit ();
 }
-
-SDL_Point *
-circle (int x, int y, int R, int *cant)
-{
-    int        count  = round (2 * M_PI * R) + 1;
-    SDL_Point *points = (SDL_Point *)malloc (sizeof (SDL_Point) * count);
-
-    for (int i = 0; i < count; ++i)
-    {
-        points[i].x = 0;
-        points[i].x = R;
-    }
-
-    SDL_Point p        = { 0, R };
-    SDL_Point anterior = p;
-    int       d        = 0;
-    int       k        = 0;
-
-    SDL_Point symmetry[8]
-        = { { p.x, p.y }, { p.y, p.x }, { p.x, -p.y }, { -p.y, p.x } };
-    for (int i = 0; i < 4; ++i)
-    {
-        points[k++] = symmetry[i];
-    }
-
-    for (int j = 0; j < R / M_SQRT2; ++j)
-    {
-        // Tomar h = d - 1/4 asegura aritmetica entera;
-        d   = D_NUEVO (anterior.x, anterior.y, d - 0.25);
-        p.x = anterior.x + 1;
-        p.y = (d < 0) ? anterior.y : anterior.y - 1;
-
-        SDL_Point symmetry[8]
-            = { { p.x, p.y },   { p.y, p.x },   { p.x, -p.y }, { -p.y, p.x },
-                { -p.x, -p.y }, { -p.y, -p.x }, { -p.x, p.y }, { p.y, -p.x } };
-        for (int i = 0; i < 8; ++i)
-            points[k++] = symmetry[i];
-
-        anterior = p;
-    }
-
-    // Trasladar por el vector centro
-    for (int i = 0; i < count; ++i)
-    {
-        points[i].x += x;
-        points[i].y += y;
-    }
-
-    *cant = k;
-    return points;
-}
-
-// SDL_Point *
-// fill_poly (SDL_Point *poly)
-// {}
